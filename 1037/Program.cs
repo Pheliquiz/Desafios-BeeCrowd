0f64a8b2113360cafd246d149615e3a5e9a82654
Gustavo Felix <ghfs102030@gmail.com>
2023-01-12 12:40:04 -0300

    12-01

---
+++ 1037/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+class URI{
+
+    static void Main(string[] args){
+
+        double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        // (0,25) (25,50) (50,75) (75,100)
+        if ((numero < 0 ) || (numero > 100)){
+            Console.WriteLine("Fora de intervalo");
+        }
+
+        if ((numero > 0) && (numero <= 25)){
+            Console.WriteLine("Intervalo [0,25]");
+        }
+
+        if ((numero > 25) && (numero <= 50)){
+            Console.WriteLine("Intervalo (25,50]");
+        }
+
+        if ((numero > 50) && (numero <= 75)){
+            Console.WriteLine("Intervalo (50, 75]");
+        }
+
+        if ((numero > 75) && (numero <= 100)){
+            Console.WriteLine("Intervalo (75,100]");
+        }
+    }
+}
---
+++ 1038/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; using System.Globalization;
+
+class URI{
+
+    static void Main(string[] args){
+
+        var entrada = Console.ReadLine().Split(' ');
+
+        int X = int.Parse(entrada[0]);
+        int Y = int.Parse(entrada[1]);
+
+        // 1 - cachorro quente R$4.00
+        if (X == 1){
+            Console.WriteLine("Total: R$ " + (Y * 4.00).ToString("F2", CultureInfo.InvariantCulture));
+        }
+        // 2 - X-Salada  R$4.50
+        if (X == 2){
+            Console.WriteLine("Total: R$ " + (Y * 4.50).ToString("F2", CultureInfo.InvariantCulture));
+        }
+        // 3 - X-Bacon R$5.00
+        if (X == 3){
+            Console.WriteLine("Total: R$ " + (Y * 5.00).ToString("F2", CultureInfo.InvariantCulture));
+        }
+        // 4 - Torrada simples R$2.00
+        if (X == 4){
+            Console.WriteLine("Total: R$ " + (Y * 2.00).ToString("F2", CultureInfo.InvariantCulture));
+        }
+        // 5 Refrigerante R$1.50
+        if (X == 5){
+            Console.WriteLine("Total: R$ " + (Y * 1.50).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
